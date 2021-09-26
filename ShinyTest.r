9c0ac54783a0d8ada7964dd63c0fe7e78ca5fc9f
ItzSylex <brandon162000@gmail.com>
2021-09-26 17:52:38 -0600

    Documentando app

--- ShinyTest.r
+++ ShinyTest.r
@@ -1,14 +1,14 @@
 library(shiny)
 ui <- fluidPage(
-    textInput("nameInput", "Cual es tu nombre"),
-    actionButton("btn", "Presiona"),
-    titlePanel(textOutput("title_panel")),
-    title = "ShinyApp"
+    textInput("nameInput", "Cual es tu nombre"),  #Crea un input label para poner el nombre
+    actionButton("btn", "Presiona"), #Crea el boton
+    titlePanel(textOutput("title_panel")), #Define el texto para que se actualice luego de presionar el boton
+    title = "ShinyApp" #Titulo de la pagina
 )
 server <- function(input, output, session) {
 
-    observeEvent(input$btn, {
-         output$title_panel <- renderText({
+    observeEvent(input$btn, {  #Evento que se encarga de ver cuando se presiona el boton
+         output$title_panel <- renderText({  #Actualiza el texto antes definido
             paste0("Hola, ", input$nameInput )
     })
   })
